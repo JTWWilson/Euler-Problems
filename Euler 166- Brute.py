e52127570c4d10ee1acc6d6ccb38b0af4d9b7020
Fabcooldog <fabcooldog@Zoobuh.com>
2016-03-28 17:15:24 +0100

    Improved Brute and Improved Actual
    Used algebra to determine that you only actually need to brute force 8
    of the variables, cutting the search parameters.

--- Euler 166- Brute.py
+++ Euler 166- Brute.py
@@ -1,44 +1,66 @@
 def main():
+    grid = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+    possibles = []
+    n=0
+    for i in range(0,144):
+        for s1 in grid[0]:
+            for s2 in grid[1]:
+                if s1+s2<=i:
+                    for s3 in grid[2]:
+                        if s1+s2+s3<=i:
+                            for s4 in grid[3]:
+                                if sum([s1,s2,s3,s4])==i:
+                                    for s5 in grid[4]:
+                                        if s1+s5<=i:
+                                            for s6 in grid[5]:
+                                                if s2+s6<=i:
+                                                    if s1+s6<=i:
+                                                        for s7 in grid[6]:
+                                                            if s3+s7<=i:
+                                                                for s8 in grid[7]:
+                                                                    if s4+s8<=i:
+                                                                        if sum([s5,s6,s7,s8])==i:
+                                                                            for s9 in grid[8]:
+                                                                                if s1+s5+s9<=i:
+                                                                                    for s10 in grid[9]:
+                                                                                        if s2+s6+s10<=i:
+                                                                                            if s4+s7+s10<=i:
+                                                                                                for s11 in grid[10]:
+                                                                                                    if s1+s6+s11<=i:
+                                                                                                        if s3+s7+s11<=i:
+                                                                                                            for s12 in grid[11]:
+                                                                                                                if s4+s8+s12<=i:
+                                                                                                                    for s13 in grid[12]:
+                                                                                                                        if sum([s9,s10,s11,s12])==i:
+                                                                                                                            if sum([s4,s7,s10,s13])==i:
+                                                                                                                                if sum([s1,s5,s9,s13])==i:
+                                                                                                                                    for s14 in grid[13]:
+                                                                                                                                        if sum([s2,s6,s10,s14])==i:
+                                                                                                                                            for s15 in grid[14]:
+                                                                                                                                                if sum([s3,s7,s11,s15])==i:
+                                                                                                                                                    for s16 in grid[15]:
+                                                                                                                                                        n+=1
+                                                                                                                                                        if sum([s1,s2,s3,s4])==i and sum([s5,s6,s7,s8])==i and sum([s9,s10,s11,s12])==i and sum([s13,s14,s15,s16])==i and sum([s1,s5,s9,s13])==i and sum([s2,s6,s10,s14])==i and sum([s3,s7,s11,s15])==i and sum([s4,s8,s12,s16])==i and sum([s1,s6,s11,s16])==i and sum([s4,s7,s10,s13])==i:
+                                                                                                                                                            possibles.append([s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16])
+        print(i,possibles[0],possibles[-1])
+    print(len(possibles))
+    print('Fin')
+    print(possibles)
+main()
+
+def compact():
     grid = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]]
-    possibles = [[]]
-    for s1 in grid[0]:
-        for s2 in grid[1]:
-            if s1+s2<12:
-                for s3 in grid[2]:
-                    if s1+s2+s3<12:
-                        for s4 in grid[3]:
-                            print(len(possibles),possibles[-1])
-                            if sum([s1,s2,s3,s4])==12:
-                                print(s1,s2,s3,s4)
-                                for s5 in grid[4]:
-                                    if s1+s5<12:
-                                        for s6 in grid[5]:
-                                            if s2+s6<12:
-                                                if s1+s6<12:
-                                                    for s7 in grid[6]:
-                                                        if s3+s7<12:
-                                                            for s8 in grid[7]:
-                                                                if s4+s8<12:
-                                                                    if sum([s5,s6,s7,s8])==12:
-                                                                        for s9 in grid[8]:
-                                                                            if s1+s5+s9<12:
-                                                                                for s10 in grid[9]:
-                                                                                    if s2+s6+s10<12:
-                                                                                        if s4+s7+s10<12:
-                                                                                            for s11 in grid[10]:
-                                                                                                if s1+s6+s11<12:
-                                                                                                    if s3+s7+s11<12:
-                                                                                                        for s12 in grid[11]:
-                                                                                                            if s4+s8+s12<12:
-                                                                                                                for s13 in grid[12]:
-                                                                                                                    if sum([s9,s10,s11,s12])==12:
-                                                                                                                        for s14 in grid[13]:
-                                                                                                                            for s15 in grid[14]:
-                                                                                                                                for s16 in grid[15]:
-                                                                                                                                    if sum([s1,s2,s3,s4])==12 and sum([s5,s6,s7,s8])==12 and sum([s9,s10,s11,s12])==12 and sum([s13,s14,s15,s16])==12 and sum([s1,s5,s9,s13])==12 and sum([s2,s6,s10,s14])==12 and sum([s3,s7,s11,s15])==12 and sum([s4,s8,s12,s16])==12 and sum([s1,s6,s11,s16])==12 and sum([s4,s7,s10,s13])==12:
-                                                                                                                                        possibles.append([s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16])
-    print(len(possibles))
-main()
+    loopVars = [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16]
+    possibles = []
+    for i
+    for j in loopVars:
+        for loopVars[j] in grid[j]:
+            if sum([s1,s2,s3,s4])==i and sum([s5,s6,s7,s8])==i and sum([s9,s10,s11,s12])==i and sum([s13,s14,s15,s16])==i and sum([s1,s5,s9,s13])==i and sum([s2,s6,s10,s14])==i and sum([s3,s7,s11,s15])==i and sum([s4,s8,s12,s16])==i and sum([s1,s6,s11,s16])==i and sum([s4,s7,s10,s13])==i:
+                possibles.append([s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16])
+    print(possibles)
--- Euler 166.py
+++ Euler 166.py
@@ -1,63 +1,67 @@
-def check(l):
-    """
-        If the sum of all the items in the list is twelve, then it returns True, if not, False
-        :argument l: The list that is checked
-        :returns True/False: Whether it sums to twelve or not
-        """
-    if sum(l) == 12:
-        return True
-    else:
-        return False
+"""
+Algebra
+Call the grid
+a,b,c,d,
+e,f,g,h,
+i,j,k,l,
+m,n,o,p
+call the sum of each line x
 
-
-class Grid:
-    def __init__(self, x, fill):
-        """
-        Creates a square grid with a given number of squares on each axis
-        :argument x: The number of squares on the x axis and therefore y axis
-        :argument fill: The thing to fill each square with
-        """
-        self.grid = []
-# Populates the grid with 'fill' in each square
-        for j in range(0, x):
-            self.grid.append([])
-            for k in range(0, len(self.grid)):
-                self.grid[k].append(fill)
-        self.rows = []
-# Sets rows to a list of each row
-        row = []
-        for x in self.grid:
-            for y in x:
-                row.append(y)
-            self.rows.append(row)
-            row = []
-        self.columns = []
-# Sets columns to a list of each column
-        column = []
-        for x in range(0, len(self.grid[0])):
-            for y in range(0, len(self.grid)):
-                column.append(self.grid[y][x])
-            self.columns.append(column)
-            column = []
-        self.diagonals = [[], []]
-# Sets diagonals to a list of both diagonals
-        for x in range(0, len(self.grid)):
-            for y in range(0, len(self.grid[x])):
-                if x == y:
-                    self.diagonals[0].append(self.grid[x][y])
-                if x == (len(self.grid[0])-1)-y:
-                    self.diagonals[1].append(self.grid[x][y])
-
-    def checkgrid(self):
-        """
-        Checks each row, column and diagonal to see if they sum to make 12
-        """
-        return
-
-# grid = creategrid(4, 4, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-g1 = Grid(5, '')
-# fill(grid)
-for i in g1.grid:
-    print(i)
-g1.checkgrid()
-del g1
+a,b,c,e,f,g,i,p are needed to imply the rest
+because:
+d=x-a-b-c
+h=x-e-f-g
+m=x-a-e-i
+j=x-d-g-m
+n=x-b-f-j
+o=x-p-m-n
+k=x-a-f-p
+l=x-i-j-k
+"""
+x=12
+possibles=0
+for a in range(0,10):#a
+    for b in range(0,10):#a,b
+        if a+b>x: break
+        for c in range(0,10):#a,b,c
+            d=x-a+b+c#a,b,c,d
+            if 0>d or d>9: break
+            if a+b+c+d!=x: break
+            for e in range(0,10):#a,b,c,d,e
+                if a+e>x: break
+                for f in range(0,10):#a,b,c,d,e,f
+                    if a+f>x: break
+                    if b+f>x: break
+                    if e+f>x: break
+                    for g in range(0,10):#a,b,c,d,e,f,g
+                        if c+g>x: break
+                        if d+g>x: break
+                        h=x-e-f-g#a,b,c,d,e,f,g,h
+                        if 0>h or h>9: break
+                        if d+h>x: break
+                        if e+f+g+h!=x: break
+                        for i in range(0,10):#a,b,c,d,e,f,g,h,i
+                            m=x-a-e-i#a,b,c,d,e,f,g,h,i,m
+                            if 0>m or m>9: break
+                            if a+e+i+m!=x: break
+                            j=x-d-g-m#a,b,c,d,e,f,g,h,i,j,m
+                            if 0>j or j>9: break
+                            if d+g+j+m!=x: break
+                            if i+j>x: break
+                            n=x-b-f-j#a,b,c,d,e,f,g,h,i,j,m,n
+                            if 0>n or n>9: break
+                            if b+f+j+n!=x: break
+                            if m+n<x: break
+                            for p in range(0,10):#a,b,c,d,e,f,g,h,i,j,m,n,p
+                                k=x-a-f-p#a,b,c,d,e,f,g,h,i,j,k,m,n,p
+                                if 0>k or k>9: break
+                                if a+f+k+p!=x: break
+                                l=x-i-j-k#a,b,c,d,e,f,g,h,i,j,k,l,m,n,p
+                                if 0>l or l>9: break
+                                if i+j+k+l!=x: break
+                                o=x-p-m-n#a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p
+                                if 0>o or o>9: break
+                                if m+n+o+p!=x: break
+                                print('',a,b,c,d,'\n',e,f,g,h,'\n',i,j,k,l,'\n',m,n,o,p)
+                                possibles+=1
+print(possibles)
