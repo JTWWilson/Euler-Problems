30025555a622849a85efc4c81f403b61c52f2bcb
Fabcooldog <fabcooldog@Zoobuh.com>
2016-03-29 14:29:32 +0100

    Solved Euler 1

---
+++ Euler 1.py
@@ -0,0 +1,18 @@
+"""
+The solution to Euler Project Problem 1
+https://projecteuler.net/problem=1
+"""
+
+
+def calculate():
+    """
+    Calculates the sum of all of the positive multiples of 3 and 5 that are less tha 1000
+    :returns: sum of all of the values
+    """
+    l = []
+    for i in range(0, 1000):
+        if i % 3 == 0 or i % 5 == 0:
+            l.append(i)
+    return sum(l)
+
+print(calculate())
