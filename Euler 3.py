86c40c8047e4dbe45841afc62c4a7ae48fa92205
Fabcooldog <fabcooldog@Zoobuh.com>
2016-03-30 11:48:32 +0100

    Created Solution to Euler 3
    Solution is very inefficient so I will have a look at trying a different
    tact

---
+++ Euler 3.py
@@ -0,0 +1,34 @@
+"""
+The solution to Euler Project Problem 3
+https://projecteuler.net/problem=3
+"""
+
+def calculate(num):
+    for i in range(num,1,-2):
+        if isprime(i) is True:
+            if num % i == 0:
+                print(i)
+                return i
+
+
+def isprime(n):
+    '''check if integer n is a prime'''
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2:
+        return True
+        # all other even numbers are not primes
+    if not n & 1:
+        return False
+    # range starts with 3 and only needs to go up
+    # the square root of n for all odd numbers
+    for x in range(3, int(n ** 0.5) + 1, 2):
+        if n % x == 0:
+                    return False
+    return True
+
+print(calculate(600851475143))
