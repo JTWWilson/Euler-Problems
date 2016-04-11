5b8860fc442e51736f352f44e52798d50fe04b45
Fabcooldog <fabcooldog@Zoobuh.com>
2016-04-11 18:06:26 +0100

    Improved efficiency on Euler 4

--- Euler 4.py
+++ Euler 4.py
@@ -3,13 +3,7 @@ The solution to Euler Project Problem 4
 https://projecteuler.net/problem=4
 """
 
-def calculate():
-    record = 0
-    for i in range(999, 0, -1):
-        for j in range(999, 0, -1):
-            if ispalindrome(i * j) is True and i * j > record:
-                record = i * j
-    print(record)
+
 
 def ispalindrome(n):
     if list(str(n))[::-1] == list(str(n)):
@@ -17,4 +11,10 @@ def ispalindrome(n):
     else:
         return False
 
-calculate()
+def calculate():
+    for i in range(999, 0, -1):
+        for j in range(999, 0, -1):
+            if ispalindrome(i * j) is True:
+                return i * j
+
+print(calculate())
